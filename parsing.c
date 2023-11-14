3b31ed7340ec1d37dd65e9da6861c6526895e3f5
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-11-14 13:17:44 +0100

    Update parsing.c

--- parsing.c
+++ parsing.c
@@ -54,10 +54,27 @@ int	check_arg(int argc, char **argv)
 
 int	ft_error(char **argv, char **map)
 {
-	if (!check_map_shape(argv[1], map) || !check_elements(map) \
-		|| !check_walls(map) || !check_path(map))
+	if (!check_map_shape(argv[1], map))
+	{
+		ft_printf("\033[31mError\nWrong map shape\n\033[0m");
+		return (0);
+	}
+	else if (!check_elements(map))
 	{
 		ft_printf("\033[31mError\n\033[0m");
+		ft_printf("\033[31mMissing or double elements\n\033[0m");
+		return (0);
+	}
+	else if (!check_walls(map))
+	{
+		ft_printf("\033[31mError\n\033[0m");
+		ft_printf("\033[31mSomething is wrong with your walls\n\033[0m");
+		return (0);
+	}
+	else if (!check_path(map))
+	{
+		ft_printf("\033[31mError\n\033[0m");
+		ft_printf("\033[31mThere is no valid path\n\033[0m");
 		return (0);
 	}
 	return (1);
